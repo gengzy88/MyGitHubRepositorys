1d7213391265a7d497362f9f777b0f9db6a1349a
gengzy88 <gengzy88@gmail.com>
2012-08-29 23:20:47 +0800

    myball

---
+++ MyOpenglEs10/src/com/gengzy88/myopengles10/MyBall.java
@@ -0,0 +1,17 @@
+package com.gengzy88.myopengles10;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class MyBall extends MyShape{
+
+	public MyBall() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void draw(GL10 gl) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ MyOpenglEs10/src/com/gengzy88/myopengles10/MyTexture.java
@@ -0,0 +1,13 @@
+package com.gengzy88.myopengles10;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class MyTexture extends MyShape{
+
+	@Override
+	public void draw(GL10 gl) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
